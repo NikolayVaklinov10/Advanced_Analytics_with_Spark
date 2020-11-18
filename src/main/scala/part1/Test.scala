6388fc9bd0faea31a536f51b1538a66266c5a078
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2020-11-18 23:50:13 +0000

    more testing for the code

--- src/main/scala/part1/Test.scala
+++ src/main/scala/part1/Test.scala
@@ -3,4 +3,13 @@ package part1
 object Test extends App {
   println("hello, world")
 
+  def anotherFunction(): Unit = println("Hello")
+
+  /**
+   * a line for testing the commit windows capabilities
+   */
+
+
+
+
 }
