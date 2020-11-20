eb61879d5547ac285bea34a50a9b71c494319243
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2020-11-20 19:10:04 +0000

    spark sql code

---
+++ src/main/scala/part1/SparkSQL.scala
@@ -0,0 +1,171 @@
+package part1
+
+import org.apache.spark.sql.SparkSession
+
+
+object SparkSQL {
+
+// Getting started
+  val spark = SparkSession
+    .builder()
+    .appName("Spark SQL basic example")
+    .config("spark.some.config.option", "some-value")
+    .getOrCreate()
+
+  // For implicit conversions like converting RDDs to DataFrames
+  import spark.implicits._
+
+  // find a json file to replace this
+  val dataframe = spark.read.json("src/main/resources/data/input.txt")
+
+  // Print the schema in a tree format
+  dataframe.printSchema()
+  // root
+  // |-- age: long (nullable = true)
+  // |-- name: string (nullable = true)
+
+  // Select only the "name" column
+  dataframe.select("name").show()
+  // +-------+
+  // |   name|
+  // +-------+
+  // |Michael|
+  // |   Andy|
+  // | Justin|
+  // +-------+
+
+  // Select everybody, but increment the age by 1
+  dataframe.select($"name", $"age" + 1).show()
+  // +-------+---------+
+  // |   name|(age + 1)|
+  // +-------+---------+
+  // |Michael|     null|
+  // |   Andy|       31|
+  // | Justin|       20|
+  // +-------+---------+
+
+  // Select people older than 21
+  dataframe.filter($"age" > 21).show()
+  // +---+----+
+  // |age|name|
+  // +---+----+
+  // | 30|Andy|
+  // +---+----+
+
+  // Count people by age
+  dataframe.groupBy("age").count().show()
+  // +----+-----+
+  // | age|count|
+  // +----+-----+
+  // |  19|    1|
+  // |null|    1|
+  // |  30|    1|
+  // +----+-----+
+
+  // Register the DataFrame as a SQL temporary view
+  dataframe.createOrReplaceTempView("people")
+
+  val sqlDF = spark.sql("SELECT * FROM people")
+  sqlDF.show()
+  // +----+-------+
+  // | age|   name|
+  // +----+-------+
+  // |null|Michael|
+  // |  30|   Andy|
+  // |  19| Justin|
+  // +----+-------+
+
+  // Global Temporary View
+  // it will disappear if the session that creates it terminates 'global_temp.view1'
+  dataframe.createGlobalTempView("people")
+
+  // Global temporary view is tied to a system preserved database `global_temp`
+  spark.sql("SELECT * FROM global_temp.people").show()
+  // +----+-------+
+  // | age|   name|
+  // +----+-------+
+  // |null|Michael|
+  // |  30|   Andy|
+  // |  19| Justin|
+  // +----+-------+
+
+  // Global temporary view is cross-session
+  spark.newSession().sql("SELECT * FROM global_temp.people").show()
+  // +----+-------+
+  // | age|   name|
+  // +----+-------+
+  // |null|Michael|
+  // |  30|   Andy|
+  // |  19| Justin|
+  // +----+-------+
+
+  // Creating Datasets
+  case class Person(name: String, age: Long)
+
+  // Encoders are created for case classes
+  val caseClassDS = Seq(Person("Andy", 32)).toDS()
+  caseClassDS.show()
+  // +----+---+
+  // |name|age|
+  // +----+---+
+  // |Andy| 32|
+  // +----+---+
+
+  // Encoders for most common types are automatically provided by importing spark.implicits._
+  val primitiveDS = Seq(1, 2, 3).toDS()
+  primitiveDS.map(_ + 1).collect() // Returns: Array(2, 3, 4)
+
+  // DataFrames can be converted to a Dataset by providing a class. Mapping will be done by name
+  val path = "src/main/resources/people.json"
+  val peopleDS = spark.read.json(path).as[Person].show()
+
+  // Inferring the Schema Using Reflection
+  // Create an RDD of Person objects from a text file, it to a Dataframe
+  val peopleDF = spark.sparkContext
+    .textFile("src/main/resources/data/input.txt")
+    .map(_.split(","))
+    .map(attributes => Person(attributes(0), attributes(1).trim.toInt))
+    .toDF()
+  // Register the DataFrame as a temporary view
+  peopleDF.createGlobalTempView("people")
+
+  // SQL statements can be run by using the sql methods provided by Spark
+  val teenagerDF = spark.sql("SELECT name, age FROM people WHERE age BETWEEN 13 AND 19")
+
+  // The columns of a row in the result can be accessed by field index
+  teenagerDF.map(teenager => "Name: " + teenager(0)).show()
+  // +------------+
+  // |       value|
+  // +------------+
+  // |Name: Justin|
+  // +------------+
+
+  // or by field name
+ teenagerDF.map(teenager => "Name: " + teenager.getAs[String]("name")).show()
+  // +------------+
+  // |       value|
+  // +------------+
+  // |Name: Justin|
+  // +------------+
+
+  // No pre-defined encoders for Dataset[Map[K,V]], define explicitly
+  implicit val mapEncoder = org.apache.spark.sql.Encoders.kryo[Map[String, Any]]
+  // Primitive types and case classes can be also defined as
+  // implicit val stringIntMapEncoder: Encoder[Map[String, Any]] = ExpressionEncoder()
+
+  // row.getValuesMap[T] retrieves multiple columns at once into a Map[String, T]
+  teenagerDF.map(teenager => teenager.getValuesMap[Any](List("name", "age"))).collect()
+  // Array(Map("name" -> "Justin", "age" -> 19))
+
+
+
+
+
+
+
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+}
