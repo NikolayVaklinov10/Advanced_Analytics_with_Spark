611c8ba66f773d913d7f61f4fbbf1c2f3c858dfe
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2020-11-25 13:45:46 +0000

    the usual spark session and creating the path to the data

--- src/main/scala/part2/RunRecommender.scala
+++ src/main/scala/part2/RunRecommender.scala
@@ -1,8 +1,20 @@
 package part2
 
+import org.apache.spark.sql.SparkSession
+
 object RunRecommender {
 
   // An application for music recommendations
+  val spark = SparkSession.builder().getOrCreate()
+  // Optional, but may help avoid errors due to long lineage
+  spark.sparkContext.setCheckpointDir("hdfs:///tmp/")
+
+  val base = "hdfs:///user/ds/"
+  val rawUserArtistData = spark.read.textFile(base + "user_artist_data.txt")
+  val rawArtistData = spark.read.textFile(base + "artist_data.txt")
+  val rawArtistAlias = spark.read.textFile(base + "artist_alias.txt")
+
+
 
 
 }
