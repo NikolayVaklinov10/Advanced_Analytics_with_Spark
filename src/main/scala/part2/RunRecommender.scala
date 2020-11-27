41de5bfeccde5899e3629094c1564523a1c24124
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2020-11-27 12:56:48 +0000

    the run recommender class

--- src/main/scala/part2/RunRecommender.scala
+++ src/main/scala/part2/RunRecommender.scala
@@ -1,20 +1,41 @@
 package part2
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Dataset, SparkSession}
 
 object RunRecommender {
 
+  def main(args: Array[String]): Unit = {
   // An application for music recommendations
   val spark = SparkSession.builder().getOrCreate()
   // Optional, but may help avoid errors due to long lineage
   spark.sparkContext.setCheckpointDir("hdfs:///tmp/")
 
-  val base = "hdfs:///user/ds/"
+  val base = "src/main/resources/profiledata_06-May-2005"
   val rawUserArtistData = spark.read.textFile(base + "user_artist_data.txt")
   val rawArtistData = spark.read.textFile(base + "artist_data.txt")
   val rawArtistAlias = spark.read.textFile(base + "artist_alias.txt")
 
+  }
 
+}
+
+class RunRecommender(private val spark: SparkSession) {
+
+  import spark.implicits._
+
+  def preparation(
+                 rawUserArtistData: Dataset[String],
+                 rawArtistData: Dataset[String],
+                 rawArtistAlias: Dataset[String]): Unit = {
+    rawArtistData.take(5).foreach(println)
+
+    val userArtistDF = rawUserArtistData.map{ line =>
+      val Array(user, artist, _*) = line.split(' ')
+      (user.toInt, artist.toInt)
+    }.toDF("user", "artist")
+
+
+  }
 
 
 }
